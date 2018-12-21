c88cbb4b6fbeefdf1264b1995df4920886bb0424
Daniel <dan1114ho@gmail.com>
2018-12-21 14:35:02 -0700

    initial commit

---
+++ node-api/src/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const database = require('./lib/database');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var customersRouter = require('./routes/customers');
+var guidRouter = require('./routes/generate_guid');
+var customerRouter = require('./routes/customer');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/customers', customersRouter);
+app.use('/generate_guid', guidRouter);
+app.use('/customer', customerRouter);
+
+module.exports = app;
